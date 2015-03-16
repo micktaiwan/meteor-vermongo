df840db09aa62306c6f4eaa60f461b196b2a7ecc
Mickael Faivre-Macon <faivrem@gmail.com>
2015-03-16 21:16:24 +0100

    insert hook, first version

--- mickaelfm_vermongo.js
+++ mickaelfm_vermongo.js
@@ -1,1 +1,16 @@
-// Write your package code here!
+Vermongo = function (options) {
+  console.log('[Vermongo] constructor');
+  this.collections = [];
+  return this;
+};
+
+Vermongo.prototype.add = function (collection) {
+  console.log('[Vermongo] adding', collection._name);
+  this.collections.push(collection);
+  collection.before.insert(function(userId, doc) {
+    doc.createdAt = Date.now();
+    doc._version = 1;
+  });
+  console.log('[Vermongo] collections count:', this.collections.length);
+  return this;
+};
--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Implementing vermongo',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/micktaiwan/meteor-vermongo.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,10 +13,13 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('WINDOWS-PREVIEW@0.3.0');
   api.addFiles('mickaelfm_vermongo.js');
+  api.use('matb33:collection-hooks');
+  api.export('Vermongo');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('mickaelfm:vermongo');
+
   api.addFiles('mickaelfm_vermongo-tests.js');
 });
