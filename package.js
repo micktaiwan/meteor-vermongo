1e089147f3e96d08c6ce38774ce10f81fd67b5af
Mickael Faivre-Macon <faivrem@gmail.com>
2015-03-17 22:06:26 +0100

    adding versions to dependant packages to be able to publish package on atmosphere

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'mickaelfm:vermongo',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Implementing vermongo',
+  summary: 'Add versions to your documents. Implementing vermongo.',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/micktaiwan/meteor-vermongo.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -14,17 +14,17 @@ Package.onUse(function(api) {
   api.versionsFrom('WINDOWS-PREVIEW@0.3.0');
   api.versionsFrom('METEOR@1.0.3.2');
   api.addFiles('vermongo.js');
-  api.use('matb33:collection-hooks');
-  api.use('dburles:collection-helpers');
+  api.use('matb33:collection-hooks@0.7.11');
+  api.use('dburles:collection-helpers@0.1.6');
 
   api.export('Vermongo');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('mickaelfm:vermongo');
-  api.use('matb33:collection-hooks');
-  api.use('dburles:collection-helpers');
+  api.use('mickaelfm:vermongo@0.0.1');
+  api.use('matb33:collection-hooks@0.7.11');
+  api.use('dburles:collection-helpers@0.1.6');
 
   api.addFiles('vermongo-tests.js');
 });
