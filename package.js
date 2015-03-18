ff13824db0b0a99066c04eceb5043756f07d8ab5
Mickael Faivre-Macon <faivrem@gmail.com>
2015-03-18 07:34:36 +0100

    bump version to 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mickaelfm:vermongo',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Add versions to your documents. Implementing vermongo.',
   // URL to the Git repository containing the source code for this package.
